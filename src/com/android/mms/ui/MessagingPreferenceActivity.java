0890fa61021166be557bc9efd2a89bf3e8c65fbc
mnazim <mnm9994u@gmail.com>
2012-12-29 12:26:53 -0700

    Add font size to Message threads
    Change-Id: I71d8a1476d39fb8a7e1de3a3db8c5ed5be0d7d65
    
    Conflicts:
    
    	res/values/arrays.xml
    	res/xml/preferences.xml
    	src/com/android/mms/ui/MessagingPreferenceActivity.java
    
    Conflicts:
    	res/values/arrays.xml

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -113,8 +113,10 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String QM_CLOSE_ALL_ENABLED      = "pref_key_close_all";
     public static final String QM_DARK_THEME_ENABLED     = "pref_dark_theme";
 
+    public static final String MESSAGE_FONT_SIZE         = "pref_key_mms_message_font_size";
+
     // Menu entries
-    private static final int MENU_RESTORE_DEFAULTS    = 1;
+    private static final int MENU_RESTORE_DEFAULTS       = 1;
 
     private Preference mSmsLimitPref;
     private Preference mSmsDeliveryReportPref;
