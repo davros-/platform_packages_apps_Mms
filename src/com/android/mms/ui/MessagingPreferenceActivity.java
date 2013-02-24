9ea72a2c92016f83a9c4cd0d17126e0b12f7534b
Kenneth Lorthioir <ibelimb@gmail.com>
2013-02-23 20:10:09 -0500

    Add quick emoji button next to text input
    This allows for a user selectable button to be displayed next to the
    SMS text input area to allow quick access to the emoji icons.
    
    Seeing as how the current process to add an emoji requires going through
    the menu and then selecting the option, this allows for frequent users
    of emojis a much simpler and time saving method.
    
    This modification is based heavily off the AOKP source
    introduced by Roman Birg.
    
    New drawable: Jacob Müller
    
    Change-Id: I6fab11f155c5c112ed39785179093cecaa5500f6
    
    Conflicts:
    
    	res/values/cm_strings.xml
    	res/xml/preferences.xml
    	src/com/android/mms/ui/MessagingPreferenceActivity.java

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -71,6 +71,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     // Emoji and Unicode
     public static final String ENABLE_EMOJIS             = "pref_key_enable_emojis";
     public static final String STRIP_UNICODE             = "pref_key_strip_unicode";
+    public static final String ENABLE_QUICK_EMOJIS       = "pref_key_enable_quick_emojis";
 
     // Split sms
     public static final String SMS_SPLIT_COUNTER        = "pref_key_sms_split_counter";
