786194f07961a97722250539c01fe5a6e27a5ed6
davros <davros@droidconcepts.com>
2013-06-05 03:49:13 -0400

    Revert "Revert "Mms: Go TRDS 2.0""
    This reverts commit 38d88f676056e112622835f9677f2458b21cf69a.

--- src/com/android/mms/quickmessage/QuickMessagePopup.java
+++ src/com/android/mms/quickmessage/QuickMessagePopup.java
@@ -37,7 +37,6 @@ import android.os.Parcelable;
 import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract.Profile;
-import android.provider.Settings;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.text.InputFilter;
